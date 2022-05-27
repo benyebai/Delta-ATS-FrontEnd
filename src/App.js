f2067e23c240798a755cf18b9a249778d06f3573
albertzed <mralbertzheng@gmail.com>
2022-05-27 14:17:30 -0700

    making register work better

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import LoginForm from "./LoginForm";
-import { RegisterForm } from "./RegisterForm";
+import { RegisterFormFirst } from "./registerForm/RegisterForm.js";
+import { RegisterFormExtras } from "./registerForm/RegisterForm.js";
 
 function App() {
   return (
     <div>
-      <RegisterForm />
+      <RegisterFormFirst />
     </div>
   );
 }
