6afb63258558d2ef5a1aeb3955d4dadc2861f465
Pojj <williambsong@gmail.com>
2022-05-31 11:01:19 -0700

    Fixed class naming

--- src/Landing.js
+++ src/Landing.js
@@ -1,37 +1,38 @@
 import React from "react";
 import "./Landing.css";
 import { Image } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 export default class Landing extends React.Component {
   render() {
     //this is the stuff at the top of the entire landing page
     //just sort of made sense to make it an object of some sort so its easier to move around
     let topPart = (
-      <div className="topPart">
+      <div className="top-part">
         <div className="logo">
-          <a href="https://deltacontrols.com/" className="logoLink">
-            <Image src="./DeltaLogo.png" className="logoImg" />
+          <a href="https://deltacontrols.com/" className="logo-link">
+            <Image src="./DeltaLogo.png" className="logo-img" />
           </a>
         </div>
-        <div className="navButtons">
-          <a href="https://www.google.com/" className="topButton textButton">
+        <div className="nav-buttons">
+          <a href="https://www.google.com/" className="top-button text-button">
             Register
           </a>
 
-          <a href="/submission" className="topButton textButton">
+          <a href="/submission" className="top-button text-button">
             Login
           </a>
 
           <a
             href="https://deltacontrols.com/about/"
-            className="topButton textButton"
+            className="top-button text-button"
           >
             About
           </a>
 
           <a
             href="https://deltacontrols.com/contact-us/"
-            className="topButton textButton"
+            className="top-button text-button"
           >
             Contact Us
           </a>
@@ -44,19 +45,21 @@ export default class Landing extends React.Component {
         {topPart}
 
         <div className="content">
-          <div className="titleBox">
+          <div className="title-box">
             <h1 className="title">Catchphrase that's as long as this</h1>
           </div>
           <div className="divider" /> <div className="divider" />{" "}
           <div className="divider" />
-          <div className="textBox">
-            <p className="bottomText">
+          <div className>
+            <p className="bottom-text">
               as an example: among the industries most <br />
               respected, Delta Controls offeres you the <br />
               coolest career path possible
             </p>
           </div>
-          <button className="register rounded-pill">Apply Now</button>
+          <Button variant="danger" className="register rounded-pill">
+            Apply Now
+          </Button>
         </div>
       </div>
     );
