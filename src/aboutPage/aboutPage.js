bda8dc461264dbbf604dc8edf92ea9ec6682ba19
trisexual <mrthomaszheng@gmail.com>
2022-06-07 23:32:37 -0700

    halfway done the styling

--- src/aboutPage/aboutPage.js
+++ src/aboutPage/aboutPage.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./aboutPage.css";
 import axios from "axios";
-import { Button, Container, Row, Col } from "react-bootstrap";
+import { Button, Container, Row, Col, Image } from "react-bootstrap";
+import Header from "../components/Header";
 
 export class AboutPage extends React.Component {
     constructor(props) {
@@ -9,19 +10,25 @@ export class AboutPage extends React.Component {
 
         //state will be updated in component did mount, where we get info from database
         this.state = {
-            email: "cool@gmail.com",
+            email: "throwtehcheasdasdsd@gmail.com",
             password: "password123",
             firstName: "Thomas",
             lastName: "Zheng",
             phoneNum: "7786969696",
             country: "Canada",
             province: "British Columbia",
-            city:"Surrey",
+            city:"New Westminster",
             postalCode:"V4N0Z3",
-            address: "Address",
-            pronouns:"He/Him"
+            address: "56-16233 83 ave more stuff more stuff",
+
+            changingEmail : false,
+            changingContact: false,
+            changingPass: false,
         }
-    
+        
+        this.handleTextChange = this.handleTextChange.bind(this);
+        this.buttonPress = this.buttonPress.bind(this);
+        
     }
 
     async componentDidMount(){
@@ -35,26 +42,40 @@ export class AboutPage extends React.Component {
         })
     }
 
+    handleTextChange(e) {
+        let changed = e.target.id;
+        this.setState({
+            [changed]:e.target.value
+        });
+    }
+
+    buttonPress(pressed){
+        console.log(this.state)
+        this.setState((prevState) => ({
+            [pressed] : !prevState[pressed]
+        }));
+    }
+
     render() {
         let allInfo = [];
         Object.keys(this.state).forEach(item => {
             allInfo.push(<h1>{this.state[item]}</h1>);
         });
 
-        let contactInfo = ["firstName", "lastName", "pronouns", "country", "province", "city", "postalCode", "phoneNum", "address"];
+        let contactInfo = ["firstName", "lastName", "phoneNum", "country", "province", "city", "postalCode", "address"];
         //this next list just contains the information in a more reader friendly way
-        let contactInfoCooler = ["First Name", "Last Name", "Pronouns" , "Country", "Province/State", "City", "Postal Code", "Phone Number", "Address", ];
-        let splits = [[],[],[],[]] // finished react objects go in here
+        let contactInfoCooler = ["First Name", "Last Name", "Phone Number", "Country", "Province/State", "City", "Postal/Zip Code", "Address", ];
+        let splits = [[],[],[],[],[]] // finished react objects go in here
         let finished = []
         let currentSplit = 0;
         for (let i = 0; i < contactInfo.length; i++){
-            if(i == 3 || i == 6 || i == 8){
+            if(i == 3 || i == 5 || i == 7 ||i == 8){
                 currentSplit += 1;
             }
 
             let current = contactInfo[i];
             let piece = ( 
-            <Col>
+            <Col lg = {true}>
                 <div className={current + " genericInfo"}>
                     <h3 className="labelText">
                         {contactInfoCooler[i]}
@@ -69,48 +90,76 @@ export class AboutPage extends React.Component {
             splits[currentSplit].push(piece);
         }
 
+        //what the email thing will look like while editing
+        let emailEditing = (
+            <div>
+                <input
+                    placeholder="Email"
+                    type="text"
+                    size="100px"
+                    onChange={this.handleChange}
+                    id="email"
+                    value = {this.state.email}
+                />
+            </div>
+        );
+
         return (
             <div>
-                <div className = "importantInfo">
-                    <div className="genericInfo">
-                        <div className = "generic"> 
-                            <h3 className="labelText">
-                                Email
-                            </h3>
+                <Header />
+                <div className = "profileContainer">
+                    <div className = "important-info">
+                        <Image 
+                        src = "https://assets.bonappetit.com/photos/57aca47753e63daf11a4d904/1:1/w_2807,h_2807,c_limit/watermelon-with-yogurt-and-fried-rosemary.jpg" 
+                        fluid
+                        className="profile-pic"
+                        roundedCircle
+                        />
+                        <div className="emailInfo emailText">
+                            <div className = "generic"> 
+                                <h3 className="labelText">
+                                    Email
+                                </h3>
+                            </div>
+
+                            {this.state.changingEmail ? emailEditing : this.state.email}
+                            
                         </div>
-
-                        <h2 className = "infoText">
-                            {this.state.email}
-                        </h2>
+                        <div className = "changeButtons">
+                            <Button className = "changeButton Email" variant="danger" onClick = {() => (this.buttonPress("changingEmail"))}>
+                                Edit
+                            </Button>
+                            <Button className="changeButton Password" variant = "danger">
+                                Change Password
+                            </Button>
+                        </div>
                     </div>
-                    <div className = "changeButtons">
-                        <Button className = "changeButton Email">
-                            Edit
-                        </Button>
-                        <Button className="changeButton Password">
-                            Change Password
-                        </Button>
-                    </div>
-                </div>
 
-                <div className = "contactInfo">
-                    <Container>
-                        <Row>
-                            {splits[0]}
+                    <div className = "contactInfo">
+                        <Container>
+                            <Row>
+                                {splits[0]}
+                            </Row>
+                            <Row>
+                                {splits[1]}
+                            </Row>
+                            <Row>
+                                {splits[2]}
+                            </Row>
+                            <Row>
+                                {splits[3]}
+                            </Row>
+                            <Row>
+                                message when you fail
+                            </Row>
+                            <Row>                            
+                            <Button variant="danger">
+                                Edit
+                            </Button>
                         </Row>
-                        <Row>
-                            {splits[1]}
-                        </Row>
-                        <Row>
-                            {splits[2]}
-                        </Row>
-                        <Row>
-                            {splits[3]}
-                        </Row>
-                    </Container>
-                    <Button>
-                        Edit
-                    </Button>
+                        </Container>
+
+                    </div>
                 </div>
             </div>
         );
