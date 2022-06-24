2cf17239c52102f5000cf3d3f23901421ae30fe0
trisexual <mrthomaszheng@gmail.com>
2022-06-24 13:01:24 -0700

    finishing my part for the unit testing

--- src/__tests__/ProfilePage.test.js
+++ src/__tests__/ProfilePage.test.js
@@ -1,14 +1,11 @@
-import ProfilePage from "../profile/ProfilePage.js"
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ProfilePage from "../profile/ProfilePage.js";
 import { render, screen, fireEvent } from "@testing-library/react";
-import '@testing-library/jest-dom'
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
 
 jest.mock("axios");
-
-
-
 
 it("renders without crashing", () => {
     const div = document.createElement("div");
@@ -25,10 +22,35 @@ it("Buttons renders properly", async () => {
 it("Buttons function as intended", async () => {
     render(<BrowserRouter><ProfilePage/></BrowserRouter>);
 
-    fireEvent.click(screen.getByText('Edit'));
+    fireEvent.click(screen.getAllByText("Edit")[1]);
 
-    await waitFor(() => screen.getByText('save'))
+    expect(screen.getByText("Save")).toBeInTheDocument();
+});
 
-    expect(screen.getByText("save")).toBeInTheDocument();
+it("Text fields are editable", async () => {
+    render(<BrowserRouter><ProfilePage/></BrowserRouter>);
+
+    fireEvent.click(screen.getAllByText("Edit")[1]);
+
+    fireEvent.change(screen.getByPlaceholderText("First Name"), {
+        target: { value: "Testing First Name" },
+      });
+    fireEvent.change(screen.getByPlaceholderText("Last Name"), {
+        target: { value: "Testing Last Name" },
+      });
+    fireEvent.change(screen.getByPlaceholderText("Postal/Zip Code"), {
+        target: { value: "Testing Postal Code" },
+      });
+      fireEvent.change(screen.getByPlaceholderText("Phone Number"), {
+        target: { value: "Testing Phone Num" },
+      });
+    fireEvent.change(screen.getByPlaceholderText("Province/State"), {
+        target: { value: "Testing Province" },
+      });
+    fireEvent.change(screen.getByPlaceholderText("City"), {
+        target: { value: "Testing City" },
+      });
+
+    expect(screen.getByText("Save")).toBeInTheDocument();
 });
 
